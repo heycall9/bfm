d85e983c0a4bb83c080db91c8173b73d319c4af2
unknown <nalanridlof@student.ub.ac.id>
2022-12-03 00:16:26 +0700

    1

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,30 +8,25 @@
     <title> <?= $data['judul']; ?></title>
 </head>
 <body>
-    <div class="text-center">
-        <img src="<?= BASEURL; ?>/img/logo-header.png" alt="logo-header" width="180" height="70" class="mt-3 mb-3 text-center img-fluid">
-    </div>
-    <nav class="navbar navbar-expand-lg shadow" style="background-color: #FF8E0A ;" >
+    <nav class="navbar navbar-expand-lg shadow" style="background-color: #0086CF ;" >
         <div class="container" >
-            <a class="navbar-brand text-white" href="<?= BASEURL; ?>">HALOFILKOM</a>
+        <a class="navbar-brand" href="#">
+      <img src="<?= BASEURL; ?>/img/vector_logo.png" alt="" width="30" height="27">
+    </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <a class="nav-link active text-white" aria-current="page" href="<?= BASEURL; ?>">Home</a>
+                <a class="nav-link active text-white" aria-current="page" href="<?= BASEURL; ?>">Beranda</a>
                 </li>                            
-                <li class="nav-item">
-                <a class="nav-link active text-white" href="<?= BASEURL; ?>/createTicket">Create Ticket</a>            
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active text-white" href="<?= BASEURL; ?>/checkTicket">Check Ticket</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active text-white" href="<?= BASEURL; ?>/about">About Us</a>
-                </li>
             </ul>
+           
+                <a class="nav-link active text-white ms-auto" aria-current="page" href="<?= BASEURL; ?>">
+            
+                    <img src="<?= BASEURL; ?>/img/vector_person.png" alt="" width="27" height="24">
+            </a>
             </div>
         </div>
     </nav>
