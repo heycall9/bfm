af52c37ea1256283ecb0368fe2e2cc7b8802852b
alahaikal <haikalskak@gmail.com>
2022-12-02 16:05:06 +0700

    homepage changes

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,4 +8,20 @@
     <title> <?= $data['judul']; ?></title>
 </head>
 <body>
-   
+<nav class="navbar navbar-expand-lg" style="background-color: #0086CF;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="<?= BASEURL; ?>">
+        <img class="img-fluid " src="<?= BASEURL; ?>/img/layout/sprout.png" alt="" width="35">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="<?= BASEURL; ?> ">Beranda</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
